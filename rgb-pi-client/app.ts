52609f66b1853da27596d863b1e87de5f3a47f65
Michael Scherbakow <ryupold@gmail.com>
2015-09-29 23:42:27 +0200

    moved some files

---
+++ rgb-pi-client/app.ts
@@ -0,0 +1,23 @@
+﻿module RGBPI {
+
+    export class Greeter {
+        count: number = 0;
+        timerToken: number;
+
+        constructor() {
+
+        }
+
+        start() {
+            this.timerToken = setInterval(() => console.log(":> "+this.count++), 500);
+        }
+
+        stop() {
+            clearTimeout(this.timerToken);
+        }
+
+    }
+}
+
+var g = new RGBPI.Greeter();
+g.start();
